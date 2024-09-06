3216c316fab1973bc57b52089ff4858b6cf97652
Shruti Panchal <spanchal1412@gmail.com>
2024-09-07 00:10:19 +0530

    CRUD complete

--- foodmenu/food/urls.py
+++ foodmenu/food/urls.py
@@ -4,8 +4,11 @@ from . import views
 
 app_name = 'food'
 urlpatterns = [
-    path('index/', views.index, name='index'),
     path('', views.home, name='home'),
-    path ('item/', views.item, name='item'),
-    path ('<int:item_id>/', views.detail, name='detail'),
+    path('index/', views.index, name='index'),
+    path('item/', views.item, name='item'),
+    path('<int:item_id>/', views.detail, name='detail'),
+    path('add', views.create_item, name='create_item'),
+    path('update/<int:id>', views.update_item, name='update_item'),
+    path('delete/<int:id>', views.delete_item, name='delete_item'),
 ]
