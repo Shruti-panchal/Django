7e9f3380512c080e831f78715a7b6a20299da5dd
Shruti Panchal <spanchal1412@gmail.com>
2024-09-06 22:14:23 +0530

    basic structure

--- foodmenu/food/models.py
+++ foodmenu/food/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Item(models.Model):
@@ -9,3 +10,5 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_des = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500, default= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8GV0f6prb-DPesR3lFwG2UsJh94yNz7ijRQ&s")
+    item_updated_time = models.DateTimeField(default=timezone.now())
--- foodmenu/food/urls.py
+++ foodmenu/food/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+
+app_name = 'food'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('index/', views.index, name='index'),
+    path('', views.home, name='home'),
     path ('item/', views.item, name='item'),
+    path ('<int:item_id>/', views.detail, name='detail'),
 ]
